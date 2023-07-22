48394eef779e300bcca319f9af454c5b9fcac2ba
HenriqueNas <henriquenas.dev@gmail.com>
2023-07-22 02:17:39 -0300

    chore: :wrench: setup platforms folders and configuration

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.bemol_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_production.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bemol_test/app/app.dart';
-import 'package:bemol_test/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
