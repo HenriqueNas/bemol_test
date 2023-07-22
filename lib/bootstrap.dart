a4a12bd583ba444a2f63ee767328162abd42f72b
HenriqueNas <henriquenas.dev@gmail.com>
2023-07-22 13:08:22 -0300

    build(bootstrap): :construction_worker: update butstrap initialization
    add registration of dependencies for application

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,12 +1,25 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:bemol_test/app/core/dependency_injection.dart';
+import 'package:bemol_test/src/data/http/http.dart';
+import 'package:bemol_test/src/data/storages/storages.dart';
+import 'package:bemol_test/src/presenter/stores/product_stores.dart';
 import 'package:flutter/widgets.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
+
+  const apiBaseUrl = String.fromEnvironment('API_URL');
+
+  dependencies
+    ..register(ProductStores())
+    ..register<LocalCacheContract<SPCacheObj>>(SPLocalCache())
+    ..register<HttpClientContract>(
+      DioHttpClient(apiBaseUrl),
+    );
 
   runApp(await builder());
 }
